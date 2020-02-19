7e8402f281580f6be1938520b676a4fb69d55b56
timu88 <wtremake88@gmail.com>
2020-02-19 16:22:59 +0900

    section 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    before_action :detect_mobile_variant
+
+    private
+    def detect_mobile_variant
+        request.variant = :mobile if request.user_agent =~ /iPhone/
+    end
 end
